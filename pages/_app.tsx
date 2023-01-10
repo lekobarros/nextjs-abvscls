b5f566b23c542525e931d12937ae188e6041a6f4
Alex Vasconcelos <lekobarros@outlook.com>
2023-01-10 10:58:51 -0300

    feat: add G-TAG Scripts.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 import localFont from '@next/font/local'
 
@@ -14,7 +15,22 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${myFont.style.fontFamily};
         }
       `}</style>
+
       <Component {...pageProps} />
+
+
+      {/* Google GTAG */}
+      <Script id="script-gtag" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_KEY}`} />
+      <Script id="script-gtag-config" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GTAG_KEY}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
     </>
   )
 }
